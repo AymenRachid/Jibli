484f90049690e50973d8ed864e35d000296d5576
AymenRachid <aymen.rachid@medtech.tn>
2018-12-05 17:42:26 +0100

    fixed demands

--- Backend/routes/Demands.js
+++ Backend/routes/Demands.js
@@ -2,42 +2,41 @@ const express = require('express')
 const router = express.Router();
 
 const Demands = require('../models/Demands')
+const User = require('../models/User');
 
 router.get('/Demands', function(req, res){
-    offers.find({}, (err, Demands)=>{
+    Demands.find({}, (err, Demands)=>{
         res.json(Demands)
     })
 })
-router.delete('/offers/:id',(req,res)=>{
-    //  console.log(req.paramas.id);
-      Demands.findByIdAndRemove({_id: req.params.id}).then(function(demand){
-          res.send(demand);
-       //   res.send({type:'Demand Deleted'})
-      });
-     
-  });
 router.get('/Demands/:id', (req, res)=>{
-.findById(req.params.id, (err, result)=>{
+    Demands.findById(req.params.id, (err, result)=>{
         res.json(result)
     })
-router.post('demands',(req,res)=>{
-var demand = new demand ({
+})
+router.delete('/demands/:id',(req,res)=>{
+    res.send({type:'Demand Deleted'})
+})
+router.post('/demands',(req,res)=>{
+    var date = new Date();
+    var demand = new Demands ({
 price: req.body.price,
 name: req.body.name,
 image: req.body.image,
-date: date.now(),
-
 description: req.body.description,
-})
-offer.save(demand, (result,err)=>{
-var id = result_id;
-User.findById(req.body.id, (result,err)=>{
-result.demands.push(id);
-result.save(result, (err, result)=>{
-    res.send(result);
+Date: date.getDate() + '/' + date.getMonth()+ '/' + date.getFullYear(),
 })
 
+demand.save(demand, (err, result)=>{
+    var id = result._id;
+    User.findById(req.body.id, (err, result)=>{
+        result.demands.push(id);
+        result.save(result, (err, result)=>{
+            res.send(result);
+        })
+    })
 })
+
 }) 
-})
+
 module.exports = router
